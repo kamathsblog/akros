020fbb8cdc5f3b80fc21650875a8e0e7405aca03
adityakamath <adityakamath@live.com>
2022-03-20 21:03:48 +0000

    added joint state publisher to mesh_publisher

--- akros_description/scripts/mesh_publisher.py
+++ akros_description/scripts/mesh_publisher.py
@@ -1,11 +1,34 @@
 #! /usr/bin/env python
 
 import rospy
+import math
 from visualization_msgs.msg import Marker, MarkerArray
+from geometry_msgs.msg import Point
+from sensor_msgs.msg import JointState
+from std_msgs.msg import Header
+
+WHEELS_X_DISTANCE = 0.093 #m
+WHEELS_Y_DISTANCE = 0.210 #m
+WHEEL_DIAMETER = 0.0762 #m 3"
 
 class AKROSMeshPublisher():
     def __init__(self):
-        self._marker_pub = rospy.Publisher("mesh_array", MarkerArray, queue_size=8)
+        self._sub_raw_vec = rospy.Subscriber("raw_vec", Point, self.set_raw_vec, queue_size=1)
+        self._marker_pub  = rospy.Publisher("mesh_array", MarkerArray, queue_size=8)
+        self._state_pub   = rospy.Publisher("joint_states", JointState, queue_size=8)
+        
+        self._raw_vec         = Point()
+        self._joint_state_msg = JointState()
+        self._velocities      = [0.0, 0.0, 0.0, 0.0] # lf, lb, rf, rb
+        self._positions       = [0.0, 0.0, 0.0, 0.0] # lf, lb, rf, rb
+        self._vel_lf          = 0.0
+        self._vel_lb          = 0.0
+        self._vel_rf          = 0.0
+        self._vel_rb          = 0.0
+        self._pos_lf          = 0.0
+        self._pos_lb          = 0.0
+        self._pos_rf          = 0.0
+        self._pos_rb          = 0.0
         
         self._marker_array_msg = MarkerArray()
         self._num_markers      = 8
@@ -82,11 +105,40 @@ class AKROSMeshPublisher():
                 self._marker.header.frame_id = self._frame_wheel_rb
                 self._marker.mesh_resource = self._url_wheel_rb
             self._marker_array_msg.markers.append(self._marker);
+            
+    def set_raw_vec(self, msg):
+        self._raw_vec = msg
         
     def run(self):
         while not rospy.is_shutdown():
+            #compute average rad/s from raw velocities using robot geometry
+            avg_rps_x = self._raw_vec.x * (2/WHEEL_DIAMETER)
+            avg_rps_y = self._raw_vec.y * (2/WHEEL_DIAMETER)
+            avg_rps_a = self._raw_vec.z * ((WHEELS_X_DISTANCE + WHEELS_Y_DISTANCE)/WHEEL_DIAMETER)
+  
+            #compute individual motor RPMs from average rad/s
+            self._velocities[0] = avg_rps_x - avg_rps_y - avg_rps_a; #lf
+            self._velocities[1] = avg_rps_x + avg_rps_y - avg_rps_a; #lb
+            self._velocities[3] = avg_rps_x - avg_rps_y + avg_rps_a; #rb
+            self._velocities[2] = avg_rps_x + avg_rps_y + avg_rps_a; #rf
+            
+            for j in range(4):
+                self._positions[j] += self._velocities[j]/5 #1/5 second frequency
+                #limit position between -pi and pi
+                if self._positions[j] > math.pi: self._positions[j] = -1*math.pi
+                if self._positions[j] < -1*math.pi: self._positions[j] = math.pi
+            
+            self._joint_state_msg.header = Header()
+            self._joint_state_msg.header.frame_id = self._frame_footprint
+            self._joint_state_msg.header.stamp = rospy.Time.now()
+            self._joint_state_msg.name = ['joint_lf', 'joint_lb', 'joint_rf', 'joint_rb']
+            self._joint_state_msg.velocity = self._velocities
+            self._joint_state_msg.position = self._positions
+            self._joint_state_msg.effort   = []
+            
+            self._state_pub.publish(self._joint_state_msg)
             self._marker_pub.publish(self._marker_array_msg)
-            rospy.rostime.wallsleep(1.0)
+            rospy.rostime.wallsleep(0.20)
             
 def main():
     try:
